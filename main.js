403acf3ccbfbc94b1bae81e6d76e7dd6870303fb
yashkumarsingh995 <60690663+yashkumarsingh995@users.noreply.github.com>
2021-03-08 01:38:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+function buttonClicked(){
+  var a1=setTimeout(headleftAppear,1000);
+  var a2=setTimeout(headRightAppear,2000);
+  var a3=setTimeout(case1appear,3000);
+  var a4=setTimeout(case1_eg_appear,4000);
+  var a5=setTimeout(case1_eg_disappear,7000);
+  var a6=setTimeout(case1disappear,7000);
+  var a7=setTimeout(case2appear,7000);
+  var a8=setTimeout(case2_1appear,8000);
+  var a9=setTimeout(case2_1_eg_appear,9000);
+  var a10=setTimeout(case2_1_eg_disappear,12000);
+  var a11=setTimeout(case2_1disappear,12000);
+  var a12=setTimeout(case2_2appear,12000);
+  var a13=setTimeout(case2_2_eg_appear,13000);
+  var a14=setTimeout(case2_2_eg_disappear,16000);
+  var a15=setTimeout(case2_2disappear,16000);
+  var a16 = setTimeout(case2_3appear, 17000);
+  var a17 = setTimeout(case2_3_eg_appear, 18000);
+  var a18 = setTimeout(case2_3_eg_disappear, 21000);
+  var a19 = setTimeout(case2_3disappear, 21000);
+  var a20=setTimeout(case2disappear,22000);
+  var a21=setTimeout(case3appear,23000);
+  var a22=setTimeout(case3_1appear,24000);
+  var a23=setTimeout(case3_1_eg_appear,25000);
+  var a24=setTimeout(case3_1_eg_disappear,29000);
+  var a25=setTimeout(case3_1disappear,29000);
+  var a26=setTimeout(case3_2appear,30000);
+  var a27=setTimeout(case3_2_eg_appear,31000);
+  var a28=setTimeout(case3_2_eg_disappear,35000);
+  var a29=setTimeout(case3_2disappear,35000);
+  var a30=setTimeout(case3disappear,35000);
+}
+
+
+
+function headRightAppear(){
+  document.getElementById('head-right').style.display="inline-block";
+}
+function headleftAppear(){
+  document.getElementById('head-left').style.display="inline-block"
+}
+function case1appear(){
+  document.getElementById('case1').style.left="0";
+}
+function case1disappear(){
+  document.getElementById('case1').style.left="-1200px";
+}
+function case2appear() {
+  document.getElementById('case2').style.left = "0";
+}
+
+function case2disappear() {
+  document.getElementById('case2').style.left = "-1200px";
+}
+function case3appear() {
+  document.getElementById('case3').style.left = "0";
+}
+
+function case3disappear() {
+  document.getElementById('case3').style.left = "-1200px";
+}
+
+
+function case2_1appear(){
+  document.getElementById('case2-1').style.right="0";
+}
+function case2_1disappear(){
+  document.getElementById('case2-1').style.right="-1200px";
+}
+function case2_2appear() {
+  document.getElementById('case2-2').style.right = "0";
+}
+
+function case2_2disappear() {
+  document.getElementById('case2-2').style.right = "-1200px";
+}
+function case2_3appear() {
+  document.getElementById('case2-3').style.right = "0";
+}
+
+function case2_3disappear() {
+  document.getElementById('case2-3').style.right = "-1200px";
+}
+function case3_1appear(){
+  document.getElementById('case3-1').style.right="0";
+}
+function case3_1disappear(){
+  document.getElementById('case3-1').style.right="-1200px";
+}
+function case3_2appear() {
+  document.getElementById('case3-2').style.right = "0";
+}
+
+function case3_2disappear() {
+  document.getElementById('case3-2').style.right = "-1200px";
+}
+function case1_eg_appear() {
+  document.getElementById('case1-eg').style.left = "0";
+}
+
+function case1_eg_disappear() {
+  document.getElementById('case1-eg').style.left = "-1200px";
+}
+function case2_1_eg_appear(){
+  document.getElementById('case2-1-eg').style.left="0";
+}
+function case2_1_eg_disappear(){
+  document.getElementById('case2-1-eg').style.left="-1200px";
+}
+function case2_2_eg_appear() {
+  document.getElementById('case2-2-eg').style.left = "0";
+}
+
+function case2_2_eg_disappear() {
+  document.getElementById('case2-2-eg').style.left = "-1200px";
+}
+function case2_3_eg_appear() {
+  document.getElementById('case2-3-eg').style.left = "0";
+}
+
+function case2_3_eg_disappear() {
+  document.getElementById('case2-3-eg').style.left = "-1200px";
+}
+
+function case3_1_eg_appear() {
+  document.getElementById('case3-1-eg').style.left = "0";
+}
+
+function case3_1_eg_disappear() {
+  document.getElementById('case3-1-eg').style.left = "-1200px";
+}
+function case3_2_eg_appear() {
+  document.getElementById('case3-2-eg').style.left = "0";
+}
+
+function case3_2_eg_disappear() {
+  document.getElementById('case3-2-eg').style.left = "-1200px";
+}
